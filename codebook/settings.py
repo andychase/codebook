d2fd32b71539892c1b695629d9d616b198e43d0f
Andy Chase <asperous2@gmail.com>
2015-07-11 12:08:33 -0700

    add template compile engine

--- codebook/settings.py
+++ codebook/settings.py
@@ -46,14 +46,20 @@ ROOT_URLCONF = 'codebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [
+        ],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
             ],
         },
     },
