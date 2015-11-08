bceb72dc16a114ded0535238450c044c13c94c7f
Andy Chase <asperous2@gmail.com>
2015-11-07 21:17:12 -0800

    Switch to whitenoise from dj-static

--- codebook/wsgi.py
+++ codebook/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codebook.settingslive")
-application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
