cea0cc456b21e8b24ff8878f20311c334fbd6a94
Andy Chase <asperous2@gmail.com>
2015-11-13 18:02:15 -0800

    Add snc logo

--- topics/settings_context.py
+++ topics/settings_context.py
@@ -1,6 +1,9 @@
 from django.contrib.sites.shortcuts import get_current_site
-
 from topics.models import TopicSite, TopicSiteData
+
+logos = {
+    'snc': 'snc.svg'
+}
 
 
 def settings_context(request):
@@ -9,12 +12,15 @@ def settings_context(request):
         user_can_edit = site.can_user_edit(request.user.id)
         user_is_admin = site.is_user_admin(request.user)
         site.css = TopicSiteData.get_css_style(get_current_site(request).id)
+        site_logo = logos.get(site.name, 'logo.svg')
     else:
         site = {}
+        site_logo = 'logo.svg'
         user_can_edit = False
         user_is_admin = False
     return {
         'site': site,
+        'site_logo': 'logos/' + site_logo,
         'user_can_edit': user_can_edit,
         'user_is_admin': user_is_admin
     }
