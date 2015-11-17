b93b29edd0ba7ba6a095412bbe8d206042e98476
Andy Chase <asperous2@gmail.com>
2015-11-16 21:05:23 -0800

    Add gzip middleware

--- codebook/settings.py
+++ codebook/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 )
 
 ROOT_URLCONF = 'codebook.urls'
