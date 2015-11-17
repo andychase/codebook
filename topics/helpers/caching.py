e9c58057f982a3097b80529450436ac628c3cce2
Andy Chase <asperous2@gmail.com>
2015-11-17 03:03:48 -0800

    Add restrictions on page cache generation
    This prevents redirects from being cached which causes problems

--- topics/helpers/caching.py
+++ topics/helpers/caching.py
@@ -12,11 +12,17 @@ def special_page_in_path(topic_name):
     return max(len(i) > 0 and i[0] == "_" for i in topic_name.strip("/").split("/"))
 
 
-def should_cache(request, path):
+def should_cache_request(request, path):
     return (
         'sessionid' not in request.COOKIES and
         not special_page_in_path(path) and
         'gzip' in request.META.get('HTTP_ACCEPT_ENCODING', [])
+    )
+
+
+def should_cache_response(response):
+    return (
+        response.status_code == 200 and not response.streaming
     )
 
 
@@ -34,7 +40,7 @@ def clear_site(site_domain):
 class TopicCacheMiddleware:
     def process_request(self, request):
         path = request.get_full_path().strip("/")
-        if should_cache(request, path):
+        if should_cache_request(request, path):
             cache_key = make_cache_key(get_current_site(request).domain, path)
             existing_cache = cache.get(cache_key)
             if existing_cache:
@@ -42,7 +48,7 @@ class TopicCacheMiddleware:
 
     def process_response(self, request, response):
         path = request.get_full_path().strip("/")
-        if should_cache(request, path):
+        if should_cache_request(request, path) and should_cache_response(response):
             cache_key = make_cache_key(get_current_site(request).domain, path)
             if hasattr(response, 'render') and callable(response.render):
                 response.add_post_render_callback(
