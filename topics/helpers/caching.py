32079c7536d4150cd8cb3b11be51efd0fb92328c
Andy Chase <asperous2@gmail.com>
2015-11-16 22:56:39 -0800

    Tweak cache key generation

--- topics/helpers/caching.py
+++ topics/helpers/caching.py
@@ -9,19 +9,19 @@ def make_cache_key(site_domain, topic_name):
 
 
 def special_page_in_path(topic_name):
-    return max(len(i) > 0 and i[0] == "_" for i in topic_name.split("/"))
+    return max(len(i) > 0 and i[0] == "_" for i in topic_name.strip("/").split("/"))
 
 
-def should_cache(request):
+def should_cache(request, path):
     return (
         'sessionid' not in request.COOKIES and
-        not special_page_in_path(request.get_full_path()) and
+        not special_page_in_path(path) and
         'gzip' in request.META.get('HTTP_ACCEPT_ENCODING', [])
     )
 
 
 def clear_topic(site_domain, topic_name):
-    cache.delete(make_cache_key(site_domain, topic_name))
+    cache.delete(make_cache_key(site_domain, topic_name.strip("/")))
 
 
 def clear_site(site_domain):
@@ -33,15 +33,17 @@ def clear_site(site_domain):
 
 class TopicCacheMiddleware:
     def process_request(self, request):
-        if should_cache(request):
-            cache_key = make_cache_key(get_current_site(request).domain, request.get_full_path())
+        path = request.get_full_path().strip("/")
+        if should_cache(request, path):
+            cache_key = make_cache_key(get_current_site(request).domain, path)
             existing_cache = cache.get(cache_key)
             if existing_cache:
                 return existing_cache
 
     def process_response(self, request, response):
-        if should_cache(request):
-            cache_key = make_cache_key(get_current_site(request).domain, request.get_full_path())
+        path = request.get_full_path().strip("/")
+        if should_cache(request, path):
+            cache_key = make_cache_key(get_current_site(request).domain, path)
             if hasattr(response, 'render') and callable(response.render):
                 response.add_post_render_callback(
                     lambda r: cache.set(cache_key, r, DEFAULT_TIMEOUT)
