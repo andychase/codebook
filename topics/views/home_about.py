50f6e3fb02cdb481eac14babf457a111205d0d31
Andy Chase <asperous2@gmail.com>
2015-11-25 18:45:14 -0800

    Terms/Privacy use unicode

--- topics/views/home_about.py
+++ topics/views/home_about.py
@@ -1,20 +1,25 @@
-from django.http import HttpResponse
+import codecs
+
+from django.http import HttpResponse, Http404
 from django.template import loader, RequestContext
 from django.utils import safestring
 from django.contrib.sites.shortcuts import get_current_site
 import markdown
 from topics.models import Topic
 
+pages = {}
+for term in {'privacy', 'terms'}:
+    with codecs.open('./topics/info/{}.md'.format(term), encoding='utf-8') as f:
+        pages[term] = markdown.markdown(f.read())
+
 
 def about(request, info_page_title):
     if info_page_title not in ['privacy', 'terms']:
-        info_page_title = "about"
-    with open('./topics/info/{}.md'.format(info_page_title)) as f:
-        page_content = markdown.markdown(f.read())
+        raise Http404("About page not found")
 
     template = loader.get_template('layouts/page.html')
     context = RequestContext(request, {
-        'content': safestring.mark_safe(page_content),
+        'content': safestring.mark_safe(pages[info_page_title]),
         'topics': [Topic.get_tree_top(get_current_site(request))],
         'title': info_page_title
     })
