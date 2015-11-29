cf4d73945db461f8fb278636636c9c5d882a76cc
Andy Chase <asperous2@gmail.com>
2015-11-29 00:30:36 -0800

    Move password reset urls to main urls.py
    This fixes the password reset utility

--- codebook/urls.py
+++ codebook/urls.py
@@ -18,11 +18,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic import RedirectView
-
 from codebook import settings
+from topics.views import users
+from django.contrib.auth import views as auth_views
+
+password_reset_confirm = \
+    "^_reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$"
 
 urlpatterns = [
                   url(r'^_admin/', include(admin.site.urls)),
+                  url(r'^_password_reset/$', users.password_reset_view, name='password_reset'),
+                  url(password_reset_confirm, auth_views.password_reset_confirm, name='password_reset_confirm'),
+                  url(r'^_password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
                   url(r'^favicon.ico$',
                       RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False)),
                   url(r'', include('topics.urls', namespace='topics')),
--- topics/urls.py
+++ topics/urls.py
@@ -7,9 +7,6 @@ from .views import topics
 from .views import topics_new
 from .views import home_about
 
-password_reset_confirm = \
-    "^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$"
-
 urlpatterns = [
     # User authentication
     url(r'^_login/$', users.login_view, name='login'),
@@ -17,9 +14,6 @@ urlpatterns = [
     url(r'^_accounts/login/$', users.login_view, name='login'),
     url(r'^_create_account/$', users.create_account_view, name='create_account'),
     url(r'^_logout/$', users.logout_view, name='logout'),
-    url(r'^_password_reset/$', users.password_reset_view, name='password_reset'),
-    url(password_reset_confirm, auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^_password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^_reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
     # About
